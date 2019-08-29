69a45c0cd68677f688ffe18fb6a9b7e3e35bd551
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2019-08-29 20:22:14 +1000

    Hover state with cards as foreign objects.

--- src/components/dataLifecycleDiagram/dataLifecycleDiagram.js
+++ src/components/dataLifecycleDiagram/dataLifecycleDiagram.js
@@ -14,11 +14,96 @@ import DataLifecycle from "./dataLifecycleDiagram.svg";
 // Styles
 import compStyles from "./dataLifecycleDiagram.module.css";
 
+let classNames = require('classnames');
+
 class DataLifecycleDiagram extends React.Component {
+
+    states = ["stateAnalysisPortals", "stateMatrixService", "stateCLIDataAccess", "stateHCADataStorage", "statePipelines", "stateStorage", "stateValidation", "stateDataCurator", "stateResearchData"];
+
+    constructor(props) {
+        super(props);
+        this.state = ({hover: false});
+    }
+
+    componentDidMount() {
+        this.setStateInteractionAndAttributes();
+    }
+
+    componentWillUnmount() {
+        this.removeStateInteractionAndAttributes();
+    };
+
+    clearActiveClassAndAttributes = (stateEl) => {
+
+        return (event) => {
+
+            // Move element up by one position in dom
+            this.moveElementUp(stateEl);
+
+            // Set hover state to false - for class "hover"
+            this.setState({hover: false});
+
+            // Remove class and filter attributes
+            stateEl.removeAttribute("class");
+            stateEl.removeAttribute("filter");
+        }
+    };
+
+    moveElementToLast = (stateEl) => {
+
+        stateEl.parentNode.appendChild(stateEl);
+    };
+
+    moveElementUp = (stateEl) => {
+
+        stateEl.parentNode.insertBefore(stateEl, stateEl.previousElementSibling);
+    };
+
+    removeStateInteractionAndAttributes = () => {
+
+        this.states.forEach(state => {
+
+            // Grab the corresponding state/container
+            let stateContainerEl = document.getElementById(`${state}`);
+
+            // Remove interaction for state/container
+            stateContainerEl.removeEventListener('mouseenter', this.setActiveClassAndAttributes(stateContainerEl));
+            stateContainerEl.removeEventListener('mouseleave', this.clearActiveClassAndAttributes(stateContainerEl));
+        })
+    };
+
+    setActiveClassAndAttributes = (stateEl) => {
+
+        return (event) => {
+
+            // Set hover state to true - for class "hover"
+            this.setState({hover: true});
+
+            // Move element down to last position of parent node
+            // Provides dom rendering precedence over all other elements and the hover overlay
+            this.moveElementToLast(stateEl);
+
+            // Add class and filter attributes
+            stateEl.setAttribute("class", compStyles.active);
+            stateEl.setAttribute("filter", 'url(#hoverShadow)')
+        }
+    };
+
+    setStateInteractionAndAttributes = () => {
+        this.states.forEach(state => {
+
+            // Grab the corresponding state/container
+            let stateContainerEl = document.getElementById(`${state}`);
+
+            // Add interaction for state/container
+            stateContainerEl.addEventListener('mouseenter', this.setActiveClassAndAttributes(stateContainerEl));
+            stateContainerEl.addEventListener('mouseleave', this.clearActiveClassAndAttributes(stateContainerEl));
+        })
+    };
 
     render() {
         return (
-            <DataLifecycle className={compStyles.dataLifecycle}/>
+            <DataLifecycle className={classNames(compStyles.dataLifecycle, {[compStyles.hover]: this.state.hover})}/>
         );
     }
 }
